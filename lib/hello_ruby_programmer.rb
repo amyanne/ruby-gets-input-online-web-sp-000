1a27ac6d43e032617f759374c16d52e54f3b9c89
Amy Westcott <amy.westcott@yahoo.com>
2019-11-26 19:31:38 +0000

    Automatically backed up by Learn

--- lib/hello_ruby_programmer.rb
+++ lib/hello_ruby_programmer.rb
@@ -1,3 +1,5 @@
 def greeting
+  puts "Hi, you! Welcome to the wonderful world of Ruby programming."
+  name = gets.chomp
   puts "Hi, #{name}! Welcome to the wonderful world of Ruby programming."
 end
